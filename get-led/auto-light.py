012ffe7b0a4c1f7f88ef4708c311789ded55600d
Аброськин Сергей <abroskin459@gmail.com>
2026-02-11 18:38:12 +0300

    added auto light script

---
+++ get-led/auto-light.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO 
+
+GPIO.setmode(GPIO.BCM)
+
+led = 26
+GPIO.setup(led, GPIO.OUT)
+res = 6
+GPIO.setup(res, GPIO.IN)
+
+while True:
+    
+    GPIO.output(led, GPIO.input(res))
