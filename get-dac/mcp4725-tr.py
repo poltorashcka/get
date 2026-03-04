bc9a8447bcfbd5ed55b0cb4c68d4a63d2ddec6c3
Аброськин Сергей <abroskin459@gmail.com>
2026-03-04 18:28:55 +0300

    adc

---
+++ get-dac/mcp4725-tr.py
@@ -0,0 +1,21 @@
+from mcp4725_driver import *
+from signal_generator import *
+import time
+
+amp = 3.2
+sig_freq = 10
+samp_freq = 1000
+
+if __name__ == "__main__":
+    try:
+        
+        dac = MCP4725(5.0)
+
+        while True:
+            voltage = float(3.2 * get_tr_wave_amp(sig_freq, time.time()))
+            dac.set_voltage(voltage)
+            wait_for_samp_per(samp_freq)
+               
+
+    finally:
+        dac.deinit()
