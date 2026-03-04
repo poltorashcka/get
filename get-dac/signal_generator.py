e1b9e098578cb879099f349fd38083048c2f670d
Аброськин Сергей <abroskin459@gmail.com>
2026-03-04 18:12:45 +0300

    add triang gen

--- get-dac/signal_generator.py
+++ get-dac/signal_generator.py
@@ -10,3 +10,8 @@ def get_sin_wave_amp(freq, time):
 def wait_for_samp_per(samp_freq):
     time.sleep(1.0/samp_freq)
 
+def get_tr_wave_amp(freq, time):
+    amp = 2 / numpy.pi * numpy.arcsin(numpy.sin(2*numpy.pi*freq*time))
+    amp += 1
+    amp /= 2
+    return amp
