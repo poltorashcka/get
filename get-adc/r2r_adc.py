d4dcff1fd1552ed83d209ba755c9e8685b05ba1e
Аброськин Сергей <abroskin459@gmail.com>
2026-03-14 11:04:49 +0300

    r2r_adc fin

--- get-adc/r2r_adc.py
+++ get-adc/r2r_adc.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 class R2R_ADC:
-    def __init__(self, dynamic_range, compare_time = 0.01, verbose = False):
+    def __init__(self, dynamic_range, compare_time = 0.007, verbose = False):
         self.dynamic_range = dynamic_range
         self.verbose = verbose
         self.compare_time = compare_time
@@ -18,7 +18,36 @@ class R2R_ADC:
         GPIO.output(self.bits_gpio, 0)
         GPIO.cleanup()
 
-    def num_to_dac(self, dac):
+    def dec2bin(self, dec):
+        return [int(bit) for bit in bin(dec)[2:].zfill(8)]
+
+    def num2dac(self, value):
+        signal = self.dec2bin(value)
+        GPIO.output(self.bits_gpio, signal)
+        return signal
+
+    def seq_count_adc(self):
+        for i in range (256):
+            self.num2dac(i)
+            time.sleep(self.compare_time)
+            if (GPIO.input(self.comp_gpio) == 1) or (i == 255):
+                return i * self.dynamic_range / 255
+
+    def get_sc_voltage(self):
+        print("voltage", self.seq_count_adc())
+
+    
+
+if __name__ == "__main__":
+    try:
+        adc = R2R_ADC(3.216)
+
+        while True:
+            adc.get_sc_voltage()
+          
+
+    finally:
+        adc.deinit()
         
 
         
